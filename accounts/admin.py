9e5bb0836c26484b0ee233cc6143c108a0e97319
Martin Bradbury <martinbradbury@btinternet.com>
2024-11-20 12:59:06 +0000

    created a user creation form and a user change from. Added extra field to database model to test later.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import *
+from .forms import *
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(CustomUser)
+
+@admin.register(CustomUser)
+class CustomAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+
+
+
 
--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
+    number = models.CharField(max_length=255)
     #tells django to use the email as the identifier and not username.
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
